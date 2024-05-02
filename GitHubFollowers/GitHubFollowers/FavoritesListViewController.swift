cc66f4146853c1173fd9045f6662dff94bbfe1ac
Alejandro <acervantest83@gmail.com>
2024-05-01 20:38:40 -0700

    Add TabBar initial setup implementation

---
+++ GitHubFollowers/GitHubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Alejandro Cervantes on 2024-05-01.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
---
+++ GitHubFollowers/GitHubFollowers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  GitHubFollowers
+//
+//  Created by Alejandro Cervantes on 2024-05-01.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+
+}
