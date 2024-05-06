f8bec2406e68bd23a1579fd0e20fe1601c605e48
Alejandro <acervantest83@gmail.com>
2024-05-05 22:43:20 -0700

    Add User Info Screen

--- GitHubFollowers/GitHubFollowers/Screens/UserInfoViewController.swift
+++ GitHubFollowers/GitHubFollowers/Screens/UserInfoViewController.swift
@@ -9,16 +9,84 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
 
+    let headerView = UIView()
+    let itemOneView = UIView()
+    let itemTwoView = UIView()
+    
+    var itemViews: [UIView] = []
+    
     var username: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureViewController()
+        layoutUI()
+        getUserInfo()
+        layoutUI()
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
-         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
-        navigationItem.rightBarButtonItem = doneButton
         
-        print(username!)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
+        
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    func getUserInfo() {
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            
+            guard let self = self else { return }
+            
+            switch result  {
+            case .success(let user):
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    func layoutUI() {
+        
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        
+        itemViews = [ headerView, itemOneView, itemTwoView]
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
+        
+        itemOneView.backgroundColor = .systemPink
+        itemTwoView.backgroundColor = .systemOrange
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemOneView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemOneView.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemTwoView.topAnchor.constraint(equalTo: itemOneView.bottomAnchor, constant: padding),
+            itemTwoView.heightAnchor.constraint(equalToConstant: itemHeight)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
 
     @objc func dismissViewController() {
---
+++ GitHubFollowers/GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Alejandro Cervantes on 2024-05-05.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
